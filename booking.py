60c7975c9d416ec27984ecf6551049e1a36da2e1
Bruno Preka <bruno.preka@imt-atlantique.net>
2021-11-10 10:50:36 +0100

    import Booking service written by Pierre

---
+++ booking.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import json
+from werkzeug.exceptions import NotFound
+
+app = Flask(__name__)
+
+#PORT = 3200
+#HOST = '192.168.0.15'
+
+with open('{}/databases/booking.json'.format("."), "r") as jsf:
+   booking = json.load(jsf)["booking"]
+
+# root message
+@app.route("/", methods=['GET'])
+def home():
+    return make_response("<h1 style='color:blue'>Welcome to the Booking service!</h1>",200)
+
+# get the complete json file
+@app.route("/bookings", methods=['GET'])
+def get_json():
+    #res = make_response(jsonify(INFO), 200)
+    res = make_response(jsonify(booking), 200)
+    return res
+
+# get bookings from a user ID
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+    for book in booking:
+        if str(book["id"]) == str(userid):
+            res = make_response(jsonify(book, discoverability(book)),200) # both movie and discoverability are JSONified
+            return res
+    return make_response(jsonify({"error":"User ID not found"}),400)
+   
+# add a booking for a user
+@app.route("/bookings/<userid>", methods=["POST"])
+def add_booking_byuser(userid):
+    req = request.get_json()
+
+    for book in booking:
+        if str(book["id"]) == str(userid):
+            return make_response(jsonify({"error":"booking already exists"},discoverability(book)),409)
+
+    booking.append(req)
+    res = make_response(jsonify({"message":"booking added"}),200)
+    return res  
+    
+def discoverability(book):
+    
+    head = "/bookings/"
+    id = book["id"]
+
+    return {
+        "possible_requests": [
+            # GET by user id
+            {
+            "method" : "GET",
+            "uri" : head + id
+            },
+        ]
+    }  
+
+if __name__ == "__main__":
+    #print("Server running in port %s"%(PORT))
+    #app.run(host=HOST, port=PORT)
+    app.run()
+    
+  
+   
